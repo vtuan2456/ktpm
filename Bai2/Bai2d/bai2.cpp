d87d0a1369df5f1fa18714d679d0b2411414778b
Văn Tuấn <147788932+vtuan2456@users.noreply.github.com>
2025-09-09 16:57:17 +0000

    bai cd

--- Bai2/Bai2d/bai2.cpp
+++ Bai2/Bai2d/bai2.cpp
@@ -2,11 +2,9 @@
 #include <cmath>
 using namespace std; 
 
-int f2(int x) {
-if (x < 10)
+int f3(int x) {
+if (log(x * x * cos(x)) < 3 * x)
 return 2 * x;
-else if (x < 2)
-return -x;
 else
 return 2 * x;
 }
@@ -15,6 +13,6 @@ int main()
 {
     int x;
     cin >> x;
-    cout << f2(x) <<endl;
+    cout << f3(x) <<endl;
     return 0;
 }
